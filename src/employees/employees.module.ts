c09123802e5cb410c8c00b6259c2f03ee358e8c4
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-06-09 17:58:07 +0330

    update modules

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesResolver } from './employees.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [EmployeesResolver, EmployeesService]
+  providers: [EmployeesResolver, EmployeesService, PrismaService]
 })
 export class EmployeesModule {}
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsResolver } from './transactions.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [TransactionsResolver, TransactionsService]
+  providers: [TransactionsResolver, TransactionsService, PrismaService]
 })
 export class TransactionsModule {}
